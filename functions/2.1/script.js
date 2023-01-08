468a741803a4bf41f724f9deb36f1b97ad033dab
Hashem <hashemabualteen@gmail.com>
2023-01-08 10:32:10 +0200

    task 2.1 is done

---
+++ functions/2.1/script.js
@@ -0,0 +1,7 @@
+const countryInfo = (country , population , capitalCity)=> {
+    return `${country} has ${population} poeple and its capital city is ${capitalCity}`;
+}
+
+console.log(countryInfo("Spain" , "47 Million" , "Madrid"));
+console.log(countryInfo("China" , "1.4 Billion" , "Beijing"));
+console.log(countryInfo("Germany" , "83 Million" , "Barlin"));
