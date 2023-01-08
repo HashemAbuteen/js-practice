91fed673603e04bdc22e64888da013740c289d5a
Hashem <hashemabualteen@gmail.com>
2023-01-08 10:50:24 +0200

    task 2.3 is done

---
+++ functions/2.3/script.js
@@ -0,0 +1,5 @@
+const circleArea = (radius)=> {
+    return Math.PI * radius * radius;
+}
+const radius = 5.5;
+console.log(`the area of a circle with radius of ${radius} is ${circleArea(radius)}`);
