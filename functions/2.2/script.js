fbb98dc5b796f03720d19a285c9d3a3a0ab330ce
Hashem <hashemabualteen@gmail.com>
2023-01-08 10:36:37 +0200

    task 2.2 is done

---
+++ functions/2.2/script.js
@@ -0,0 +1,5 @@
+const tellFortune = (jobTitle , location , partnerName ,    numOfChildren)=> {
+    return `You will be ${jobTitle} in ${location} married to ${partnerName} with ${numOfChildren} children/s`;
+}
+
+console.log(tellFortune("Pilot" , "Jerusalem" , "Meriam" , 17));
