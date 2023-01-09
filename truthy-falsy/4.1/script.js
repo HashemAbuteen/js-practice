86844985080769a42200db2544ef203d52ea6942
Hashem <hashemabualteen@gmail.com>
2023-01-09 13:03:48 +0200

    task 4.1 is done

---
+++ truthy-falsy/4.1/script.js
@@ -0,0 +1,11 @@
+const numSiblings = Number(prompt("How many siblings do you have?"));
+
+if (numSiblings === 1) {
+  console.log("You have 1 sibling.");
+} else {
+if (numSiblings > 1) {
+  console.log("You have more than 1 sibling.");
+} else {
+    console.log("You don't have any siblings.");
+  }
+}
