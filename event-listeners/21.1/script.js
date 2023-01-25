d8a88e59873037eea937fbff4fa1edee16db7c27
Hashem <hashemabualteen@gmail.com>
2023-01-25 10:57:51 +0200

    21.1 is done

---
+++ event-listeners/21.1/script.js
@@ -0,0 +1,19 @@
+const ageInput = document.getElementById("input-age");
+const ageCheckButton = document.getElementById("button-age-check");
+const resultDiv = document.getElementById("div-result");
+
+ageCheckButton.addEventListener("click" , () => {
+    const age = ageInput.value;
+    if(!isNaN(parseFloat(age))){
+        age = parseFloat(age);
+        if(age >= 18){
+            resultDiv.innerHTML = "You can drink";
+        }
+        else {
+            resultDiv.innerHTML = "You cannot drink";
+        }
+    }
+    else {
+        resultDiv.innerText = "Age must be a number";
+    }
+})
