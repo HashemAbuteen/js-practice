df8f788c43d902e26b7f26c6c0e1ec9110c2f13b
Hashem <hashemabualteen@gmail.com>
2023-01-30 11:29:58 +0200

    23.2 is done

---
+++ this/23.2/script.js
@@ -0,0 +1,10 @@
+const storm = {
+    superPower : "Flying",
+    printSuperPower : printSuperPower
+}
+function printSuperPower() {
+    console.log("my superpower is " +
+    this.superPower);
+}
+
+storm.printSuperPower();
